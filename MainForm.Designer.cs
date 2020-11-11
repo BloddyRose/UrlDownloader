5833949f72d245292f422748df2fd55888e2269e
BloddyRose <bloddy.rose.404@gmail.com>
2020-11-11 17:04:07 +0200

    Add project files.

---
+++ MainForm.Designer.cs
@@ -0,0 +1,282 @@
+﻿namespace UrlDownloader
+{
+    partial class UrlDownloader
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.UrlBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.DownloadingLbl = new System.Windows.Forms.Label();
+            this.StatusLbl = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.DownloadedLbl = new System.Windows.Forms.Label();
+            this.SpeedLbl = new System.Windows.Forms.Label();
+            this.StartBtn = new System.Windows.Forms.Button();
+            this.PauseBtn = new System.Windows.Forms.Button();
+            this.ResumeBtn = new System.Windows.Forms.Button();
+            this.AboutBtn = new System.Windows.Forms.Button();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.ProgressLbl = new System.Windows.Forms.Label();
+            this.folderBrowserDialog = new System.Windows.Forms.FolderBrowserDialog();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 82);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Informations";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(8, 4);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Url:";
+            this.toolTip1.SetToolTip(this.label2, "Please enter a url that hase and extension at the end ");
+            // 
+            // UrlBox
+            // 
+            this.UrlBox.AllowDrop = true;
+            this.UrlBox.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.UrlBox.Location = new System.Drawing.Point(46, 4);
+            this.UrlBox.Name = "UrlBox";
+            this.UrlBox.Size = new System.Drawing.Size(742, 22);
+            this.UrlBox.TabIndex = 2;
+            this.toolTip1.SetToolTip(this.UrlBox, "Please enter a url that hase and extension at the end \r\n");
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Nirmala UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(44, 103);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Status: ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Nirmala UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(43, 127);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(84, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Downloading: ";
+            // 
+            // DownloadingLbl
+            // 
+            this.DownloadingLbl.AutoSize = true;
+            this.DownloadingLbl.Font = new System.Drawing.Font("Nirmala UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DownloadingLbl.Location = new System.Drawing.Point(176, 127);
+            this.DownloadingLbl.Name = "DownloadingLbl";
+            this.DownloadingLbl.Size = new System.Drawing.Size(26, 13);
+            this.DownloadingLbl.TabIndex = 6;
+            this.DownloadingLbl.Text = "N/A";
+            // 
+            // StatusLbl
+            // 
+            this.StatusLbl.AutoSize = true;
+            this.StatusLbl.Font = new System.Drawing.Font("Nirmala UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.StatusLbl.Location = new System.Drawing.Point(176, 103);
+            this.StatusLbl.Name = "StatusLbl";
+            this.StatusLbl.Size = new System.Drawing.Size(26, 13);
+            this.StatusLbl.TabIndex = 5;
+            this.StatusLbl.Text = "N/A";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Nirmala UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(44, 169);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(77, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Downloaded:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Nirmala UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(43, 150);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(42, 13);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "Speed:";
+            // 
+            // DownloadedLbl
+            // 
+            this.DownloadedLbl.AutoSize = true;
+            this.DownloadedLbl.Font = new System.Drawing.Font("Nirmala UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DownloadedLbl.Location = new System.Drawing.Point(176, 169);
+            this.DownloadedLbl.Name = "DownloadedLbl";
+            this.DownloadedLbl.Size = new System.Drawing.Size(26, 13);
+            this.DownloadedLbl.TabIndex = 10;
+            this.DownloadedLbl.Text = "N/A";
+            // 
+            // SpeedLbl
+            // 
+            this.SpeedLbl.AutoSize = true;
+            this.SpeedLbl.Font = new System.Drawing.Font("Nirmala UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SpeedLbl.Location = new System.Drawing.Point(176, 150);
+            this.SpeedLbl.Name = "SpeedLbl";
+            this.SpeedLbl.Size = new System.Drawing.Size(26, 13);
+            this.SpeedLbl.TabIndex = 9;
+            this.SpeedLbl.Text = "N/A";
+            // 
+            // StartBtn
+            // 
+            this.StartBtn.Location = new System.Drawing.Point(243, 70);
+            this.StartBtn.Name = "StartBtn";
+            this.StartBtn.Size = new System.Drawing.Size(75, 27);
+            this.StartBtn.TabIndex = 11;
+            this.StartBtn.Text = "Start";
+            this.toolTip1.SetToolTip(this.StartBtn, "Start the downloader");
+            this.StartBtn.UseVisualStyleBackColor = true;
+            this.StartBtn.Click += new System.EventHandler(this.StartBtn_Click);
+            // 
+            // PauseBtn
+            // 
+            this.PauseBtn.Location = new System.Drawing.Point(324, 70);
+            this.PauseBtn.Name = "PauseBtn";
+            this.PauseBtn.Size = new System.Drawing.Size(75, 27);
+            this.PauseBtn.TabIndex = 12;
+            this.PauseBtn.Text = "Pause";
+            this.toolTip1.SetToolTip(this.PauseBtn, "Pause the downloader");
+            this.PauseBtn.UseVisualStyleBackColor = true;
+            this.PauseBtn.Click += new System.EventHandler(this.PauseBtn_Click);
+            // 
+            // ResumeBtn
+            // 
+            this.ResumeBtn.Location = new System.Drawing.Point(405, 70);
+            this.ResumeBtn.Name = "ResumeBtn";
+            this.ResumeBtn.Size = new System.Drawing.Size(75, 27);
+            this.ResumeBtn.TabIndex = 13;
+            this.ResumeBtn.Text = "Resume";
+            this.toolTip1.SetToolTip(this.ResumeBtn, "Resume the downloader");
+            this.ResumeBtn.UseVisualStyleBackColor = true;
+            this.ResumeBtn.Click += new System.EventHandler(this.ResumeBtn_Click);
+            // 
+            // AboutBtn
+            // 
+            this.AboutBtn.Location = new System.Drawing.Point(486, 70);
+            this.AboutBtn.Name = "AboutBtn";
+            this.AboutBtn.Size = new System.Drawing.Size(75, 27);
+            this.AboutBtn.TabIndex = 14;
+            this.AboutBtn.Text = "About";
+            this.AboutBtn.UseVisualStyleBackColor = true;
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(46, 36);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(742, 23);
+            this.progressBar.TabIndex = 15;
+            // 
+            // ProgressLbl
+            // 
+            this.ProgressLbl.AutoSize = true;
+            this.ProgressLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ProgressLbl.Location = new System.Drawing.Point(10, 41);
+            this.ProgressLbl.Name = "ProgressLbl";
+            this.ProgressLbl.Size = new System.Drawing.Size(24, 13);
+            this.ProgressLbl.TabIndex = 16;
+            this.ProgressLbl.Text = "0 %";
+            // 
+            // toolTip1
+            // 
+            this.toolTip1.IsBalloon = true;
+            this.toolTip1.ToolTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.toolTip1.ToolTipTitle = "Helper";
+            // 
+            // UrlDownloader
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 201);
+            this.Controls.Add(this.ProgressLbl);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.AboutBtn);
+            this.Controls.Add(this.ResumeBtn);
+            this.Controls.Add(this.PauseBtn);
+            this.Controls.Add(this.StartBtn);
+            this.Controls.Add(this.DownloadedLbl);
+            this.Controls.Add(this.SpeedLbl);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.DownloadingLbl);
+            this.Controls.Add(this.StatusLbl);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.UrlBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.Name = "UrlDownloader";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Url-Downloader";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox UrlBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label DownloadingLbl;
+        private System.Windows.Forms.Label StatusLbl;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label DownloadedLbl;
+        private System.Windows.Forms.Label SpeedLbl;
+        private System.Windows.Forms.Button StartBtn;
+        private System.Windows.Forms.Button PauseBtn;
+        private System.Windows.Forms.Button ResumeBtn;
+        private System.Windows.Forms.Button AboutBtn;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Label ProgressLbl;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog;
+        private System.Windows.Forms.ToolTip toolTip1;
+    }
+}
---
+++ MainForm.cs
@@ -0,0 +1,88 @@
+﻿using AltoHttp;
+using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace UrlDownloader
+{
+    public partial class UrlDownloader : Form
+    {
+        private HttpDownloader downloader;
+        public UrlDownloader()
+        {
+            InitializeComponent();
+        }
+
+        private void StartBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DialogResult box = MessageBox.Show("Info", "You need to select a folder where to save the file!", MessageBoxButtons.OKCancel);
+                if (DialogResult.OK == box)
+                {
+                    folderBrowserDialog.ShowDialog();
+                    string folder = folderBrowserDialog.SelectedPath;
+                    string file = UrlBox.Text;
+
+                    string outputpath = $"{folder}\\{Path.GetFileName(file)}";
+
+                    downloader = new HttpDownloader(file, outputpath);
+                    downloader.DownloadCompleted += Downloader_DownloadCompleted;
+                    downloader.ProgressChanged += Downloader_ProgressChanged;
+                    downloader.Start();
+
+                }
+                else
+                {
+
+                }
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show("Erorr", $"An erorr appear: {ex.Message}", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void Downloader_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            progressBar.Value = (int)e.Progress;
+            ProgressLbl.Text = $"{e.Progress.ToString("0.00")} %";
+            SpeedLbl.Text = string.Format("{0} MB/s", (e.SpeedInBytes / 1024d / 1024d).ToString("0.00"));
+            DownloadedLbl.Text = string.Format("{0} MB/s", (downloader.TotalBytesReceived / 1024d / 1024d).ToString("0.00"));
+            StatusLbl.Text = "Downloading...";
+            DownloadingLbl.Text = "Yes..";
+        }
+
+        private void Downloader_DownloadCompleted(object sender, EventArgs e)
+        {
+            Invoke((MethodInvoker)delegate
+            {
+                MessageBox.Show("Download", "Downloaded Completed", MessageBoxButtons.OK);
+
+                StatusLbl.Text = "N/A";
+                DownloadedLbl.Text = "N/A";
+                DownloadingLbl.Text = "N/A";
+                SpeedLbl.Text = "N/A";
+                progressBar.Value = 0;
+                ProgressLbl.Text = "0 %";
+            });
+        }
+
+        private void PauseBtn_Click(object sender, EventArgs e)
+        {
+            if (downloader != null)
+            {
+                downloader.Pause();
+            }
+        }
+
+        private void ResumeBtn_Click(object sender, EventArgs e)
+        {
+            if (downloader != null)
+            {
+                downloader.Resume();
+            }
+        }
+    }
+}
